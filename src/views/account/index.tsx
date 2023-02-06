968702e61359830e0937d9ccf5eff407b4de41e7
feng.wuwf <feng.wuwf@shopee.com>
2023-02-07 00:06:29 +0800

    feat: proxy & joinst

--- src/views/account/index.tsx
+++ src/views/account/index.tsx
@@ -1,7 +1,16 @@
+import axios from 'axios';
 import React from 'react';
 import Desc from "../../components/header/index";
+// import { getAccount } from './api';
 
 const AccountPage: React.FC = () => {
+  React.useEffect(() => {
+    console.log('111');
+    axios.get('/wf/admin/account').then((res) => {
+      console.log('res', res);
+    });
+  });
+
   return (
     <Desc />
   );
