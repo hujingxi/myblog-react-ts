14c7eabd8376e517cd66cb1b1fdc0ce9367956a1
feng.wuwf <feng.wuwf@shopee.com>
2023-02-07 23:39:32 +0800

    feat: post method

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,23 @@
+import axios from 'axios';
 import React from 'react';
 
 const Desc: React.FC = () => {
+
+  const postRequest = () => {
+    axios.post('/wf/admin/click', {
+      data: 'click',
+      msg: 'fsdjkl',
+    }).then(res => {
+      console.log('post', res);
+    }).catch(err => {
+      console.log('err', err);
+    });
+  }
   return (
-    <p>这是我的个人介绍</p>
+    <>
+      <p>这是我的个人介绍</p>
+      <button onClick={() => postRequest()}>Post Test</button>
+    </>
   );
 }
 
--- src/views/account/index.tsx
+++ src/views/account/index.tsx
@@ -4,15 +4,20 @@ import Desc from "../../components/header/index";
 // import { getAccount } from './api';
 
 const AccountPage: React.FC = () => {
+  const [msg, setMsg] = React.useState('');
   React.useEffect(() => {
     console.log('111');
     axios.get('/wf/admin/account').then((res) => {
       console.log('res', res);
+      setMsg(res.data);
     });
   });
 
   return (
-    <Desc />
+    <>
+      <div>{msg}</div>
+      <Desc />
+    </>
   );
 }
 
