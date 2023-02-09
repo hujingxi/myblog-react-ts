81bbff2158461e53ec723708d2d08eadb115fa1e
feng.wuwf <feng.wuwf@shopee.com>
2023-02-09 22:24:55 +0800

    add: eslint

--- src/views/account/index.tsx
+++ src/views/account/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Desc from "../../components/header/index";
+import Desc from '../../components/header/index';
 // import { getAccount } from './api';
 
 const AccountPage: React.FC = () => {
